83439f14aec5a79f55c569b93e491a7ab4b6c8b2
Sofia Valdez <valdezsofia7@gmail.com>
2022-07-29 13:34:44 -0300

    proyecto base

---
+++ routes/publicRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const publicRouter = express.Router();
+const { showHome } = require("../controllers/pagesController");
+// Rutas Públicas:
+// ...
+publicRouter.get("/home", showHome);
+module.exports = publicRouter;
