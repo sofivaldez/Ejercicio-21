99b03728437359111f20dc79e4000472e0c54406
Sofia Valdez <valdezsofia7@gmail.com>
2022-08-01 14:14:10 -0300

    vista articulo

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -4,7 +4,10 @@ const { Article } = require("../models");
 async function index(req, res) {}
 
 // Display the specified resource.
-async function show(req, res) {}
+async function show(req, res) {
+  const article = await Article.findByPk(1);
+  res.render("articleView", { article });
+}
 
 // Show the form for creating a new resource
 async function create(req, res) {}
--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const publicRouter = express.Router();
 
 const { showHome } = require("../controllers/pagesController");
+const { show } = require("../controllers/articleController");
 // Rutas Públicas:
 // ...
 
 publicRouter.get("/home", showHome);
+publicRouter.get("/article/:id", show);
 
 module.exports = publicRouter;
